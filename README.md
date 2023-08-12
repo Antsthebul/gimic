b62eb7f7c5459962133dd23bd450ad577d4a0675
antsthebul <anthony.allen.srt@gmail.com>
2023-08-11 23:11:05 -0400

    Updated example yaml file. Refactor main. Updated READ ME. improved flow

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ repo 1 <Main_repo>
 
 Easily manage specific files/directories from other repos "remotely". 
 
-## Insallation:
+## Installation:
 
 Make sure you have `git` installed, and your working in a git repo
 
@@ -27,9 +27,9 @@ Make sure you have `git` installed, and your working in a git repo
 
 ## How it works:
 
-_**Gimic**_ what designed to mi ***mic*** ***Gi***t,  where although `submodules` and `subtree` are great options for including another repo into your project, you may want a little bit more granular control of what file(s) are actually pulled in and are sitting in your local directory/workspace.  
+_**Gimic**_ was designed to mi ***mic*** ***Gi***t,  where although `submodules` and `subtree` are great options for including another repo into your project, you may want a little bit more granular control of what file(s) are actually pulled in and are sitting in your local directory/workspace. Using `gimic` only affects your repo where the file will appear untracked (as any new file would) so it may be a good idea to update your .gitignore file or .git/exclude  
 
-## Usage:
+## Configuration:
 
 Create a gloc.yaml file in the root of your poject
  - Checkout the [example configuration file](https://github.com/Antsthebul/gimic/blob/main/example.yaml) to get an idea
@@ -42,6 +42,16 @@ repo 1
 
 If you applied installation step 2.ii, this can be run anywhere within a nested .git directory, otherwise the target build directory will need to exist within the desired "root" repo.
 
+
+## Usage:
+
+`$ gimic checkout [options]`
+- This is is just a wrapper around `git pull` and `copy <source> <to>`. Specifying just the action , ie. `checkout`, and no other args means that a yaml file exists somehwere in the execution path (ie. where you've called `gimic`) and that when the alternate_repo is pulled down, the files will be copied from the `alternate_source` to the `alternate_target`. Note that at least one of the locations needs to exists, and if only one of either option does exists, this will be the default for the other option. Meaning if `!alternate_source then alternate_target = alternate_source  (and vice versa)`
+
+`$ gimic commit [options]` and `gimic push [options]`
+ - Pass all normal git options to git command, but using the alternate_target
+
+
 ## Changes
 1. Allow existence gloc.yaml file to be optional
 2. Allow downloadable prebuilt binaries for popular systems
@@ -51,4 +61,3 @@ If you applied installation step 2.ii, this can be run anywhere within a nested 
 ## Contributions
 Feel free to commment in the repo and let me know if you have any other suggestions or want to contribute! 
 
-# UNDER CONSTRUCTION
--- src/main.rs
+++ src/main.rs
@@ -1,150 +1,32 @@
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-use serde_yaml;
 
+use std::collections::VecDeque;
 use std::env;
-use std::process::Command;
-use std::path;
-use std::ffi::OsStr;
-use std::fs;
-use std::io::Write;
+use std::process;
 
-mod gimic;
-use crate::gimic::*;
+mod gimic{
+    pub mod gimic;
+    pub mod commands;
+}
 
-const TEMP_BASE_PATH: &str  = ".gump/tmp";
+use crate::gimic::gimic::*;
+
+
 fn main() {
+    let mut args: VecDeque<String> = env::args().collect();
+
     let current_dir = env::current_dir().unwrap();
-    let mut ancestors = current_dir.ancestors();
-    let mut found = false;
+    // TODO: Allow settings for verbosity
+    let config: BaseConfig;
+    if args.len() < 3{
 
-    'outer: for file in ancestors.next(){
-        for path in fs::read_dir(file).unwrap(){
-
-            let name = path.unwrap().file_name();
-            if name == "gloc.yaml" {
-                found = true;
-                break 'outer;
-            }
-        }
+        config = match verify_gloc_exists(&current_dir){
+                Ok(x) => BaseConfig::build_mapping_w_yaml(x),
+                _ =>process::exit(1)
+        };
+        create_temporary_file_store(&current_dir);  
+        args.pop_front(); // drop program name
+        let action = args.pop_front().unwrap();
+        let idx: u32 = 0;
+        config.run_action(action.as_ref(), idx, args);
     }
-    if !found{
-        failed_to_locate_gloc();
-    }
-
-    let gump_dir_path:path::PathBuf = [&current_dir, path::Path::new(".gump")].iter().collect();
-    
-    if let Err(err) = std::fs::create_dir(&gump_dir_path.as_os_str()){
-        match err.kind() {
-            std::io::ErrorKind::AlreadyExists => (),
-            _ => panic!("Unable to create .gump directory: {:?}", err)
-            }
-    }
-
-    let cache: path::PathBuf = [&gump_dir_path, path::Path::new("tmp")].iter().collect();
-
-    if let Err(err) = std::fs::create_dir(cache.as_os_str()){
-        match err.kind(){
-            std::io::ErrorKind::AlreadyExists => (),
-            _ => panic!("Unable to create tmp/ within .gump: {:?}", err)
-            }
-    }            
-        
-    
-
-    println!("\n\x1B[1m\x1b[33mWarning\x1B[0m Attempting to fetch foreign repo");
-
-
-    let config_file = fs::File::open("gloc.yaml").unwrap_or_else(|error|{
-        panic!("Unable to open file: {:?}", error);
-    });
-
-    let mut config : BaseConfig = serde_yaml::from_reader(config_file).unwrap_or_else(|error|{
-        panic!("Problem reading file: {:?}", error);
-    });
-
-    
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        println!("Usage: gimic [checkout|pull|status] [branch] <repository_url> <target_directory>\n");
-        // return;
-    }
-
-    let action = &args[1];
-    let branch = if args.len() > 4 { Some(&args[2]) } else { None };
-    if config.foreign_repo.is_none(){
-        config.foreign_repo = Some(args[args.len() - 2].to_string())
-    }
-
-    if config.foreign_target.is_none() {
-        config.foreign_target = Some(args[args.len() - 1].to_string())
-    } 
-
-
-
-    match action.as_str() {
-        "checkout" => mimic_git_checkout(branch, config.foreign_repo,  config.foreign_target.unwrap()),
-        "pull" => mimic_git_pull(config.foreign_repo, config.foreign_target.unwrap()),
-        "status" => mimic_git_status(),
-        _ => println!("Unknown action: {}", action),
-    }
-}
-
-fn run_command(program: &str, action:&str, args: &[&Option<String>]) {
-    println!("Fetching repo..\n\n[GIT]");
-
-    let status = Command::new(program)
-        .arg(action)
-        .args(args.iter().map(|item| {
-            match item {
-                Some(s) => OsStr::new(s),
-                _=> OsStr::new("")
-            } 
-        }))
-        .status()
-        .expect("Failed to execute command");
-    
-    
- 
-    if !status.success() {
-        println!("Command execution failed");
-        std::process::exit(1);
-    }
-}
-
-fn mimic_git_checkout(branch: Option<&String>, repository_url: Option<String>, target_directory: String) {
-    
-    run_command("git", "clone", &[ &repository_url, &Some(TEMP_BASE_PATH.to_owned())]);
-
-    if let Some(branch_name) = branch {
-        run_command("git","branch", &[&Some(branch_name.to_string())]);
-    }
-    
-    let temp_path = format!("{}/tests/models.py", &TEMP_BASE_PATH);
-
-    let output_directory: String = format!("{}", target_directory);
-    println!("Output => {:?}", OsStr::new(&output_directory.to_owned()));
-
-    std::fs::create_dir("test").expect("Unable to create dir");
-
-    std::fs::copy(temp_path.to_owned(),output_directory).expect("Unable to copy to specified dir");
-
-    // remove dirs because enitre repos will always be 'cloned'
-    fs::remove_dir_all(TEMP_BASE_PATH).expect("Failed to remove");
-    
-    write_green();
-}
-
-fn write_green(){
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green)));
-    writeln!(&mut stdout, "Success!");
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)));
-}
-fn mimic_git_pull(repository_url: Option<String>, target_directory: String) {
-    mimic_git_checkout(None, repository_url, target_directory);
-}
-
-fn mimic_git_status(){
-    run_command("git","status", &[]);
 }
